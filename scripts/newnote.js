187574033fc66ac6f06e8b4aab5b1093a02d4a88
emachipana <emachipana0102@gmail.com>
2022-05-20 18:18:39 -0500

    ADD: call of function renderNotes

--- scripts/newnote.js
+++ scripts/newnote.js
@@ -1,11 +1,13 @@
 const form = document.querySelector(".js-form")
 form.addEventListener("submit",(event) => {
-  event.preventDefault()
+  event.preventDefault();
   const dataNote = {
     title: event.target.textTitle.value,
     content: event.target.textArea.value,
     color: event.target.colorText.value
     }
-    initialNotes.push(dataNote)
-    localStorage.setItem("notes",JSON.stringify(initialNotes))
+
+    notes.push(dataNote);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    renderNotes(notes);
 }) 
